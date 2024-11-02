72ce300ff65ce3eee8aa5d0b47b17ef303273a05
diazkr <juliethdiaz001@gmail.com>
2024-11-02 12:17:39 -0500

    remove inputs

--- main.tf
+++ main.tf
@@ -25,9 +25,3 @@ output "fibonacci_series" {
   value = local.fibonacci_series
 }
 
-# Agregar la opción de no entrada al plan
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "terraform plan -input=false && terraform apply -input=false"
-  }
-}
