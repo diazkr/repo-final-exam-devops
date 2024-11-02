d4172ec0e123ea0e5ae19ddc18ed2082ed0a5821
diazkr <juliethdiaz001@gmail.com>
2024-11-02 12:00:00 -0500

    add the fibbonaccie

---
+++ main.tf
@@ -0,0 +1,22 @@
+# main.tf
+
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+# Definición manual de la serie de Fibonacci hasta el número 21
+locals {
+  fibonacci_series = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+}
+
+# Imprime la serie de Fibonacci en la consola
+output "fibonacci_series" {
+  value = local.fibonacci_series
+}
